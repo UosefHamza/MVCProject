8fe6f21e6265d4253e0c029fe0c23176f418e623
UosefHamza <uosefhamza0@gmail.com>
2024-09-16 00:11:47 +0300

    Part 07 Fist Migration

--- Company.Data/Contexts/CompanyDbContext.cs
+++ Company.Data/Contexts/CompanyDbContext.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Company.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,23 @@ namespace Company.Data.Contexts
 {
     public class CompanyDbContext : DbContext
     {
+        public CompanyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server =. ; database=CompanyMVCG01; trusted_connections =true; TrustServerCertificate=True");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Employee> Employees { get; init; }
+
+        public DbSet<Department> Departments { get; init; }
 
     }
 }
